0e0c241becce6963979602d4ba1dbf51777c0484
aben20807 <aben20807@gmail.com>
2017-05-19 16:30:00 +0800

    hw3 ALU, Controller, Jump_Ctrl, PC, Regfile copy from hw2

--- hw3/SRC/PC.v
+++ hw3/SRC/PC.v
@@ -1,19 +1,32 @@
 // Program Counter
 
-module PC ( clk, 
+module PC ( clk,
 			rst,
 			PCWrite,
-			PCin, 
+			PCin,
 			PCout);
-	
+
 	parameter pc_size = 18;
-	
+
 	input  clk, rst;
 	input  PCWrite;
 	input  [pc_size-1:0] PCin;
 	output [pc_size-1:0] PCout;
-	
-	// write your code in here
-		   
-endmodule
 
+	// write your code in here
+	reg [pc_size-1:0] PCout = 18'b0000_0000_0000_0000_00;
+
+	always@(negedge clk, posedge rst)
+	begin
+		if(rst)
+		begin
+			PCout <= 18'b0000_0000_0000_0000_00;
+			//$display("rst %b", PCout);
+		end
+		else
+		begin
+			PCout <= PCin;
+			//$display("call PC %b\n", PCout);
+		end
+	end
+endmodule
--- hw3/SRC/Regfile.v
+++ hw3/SRC/Regfile.v
@@ -1,6 +1,6 @@
 // Regfile
 
-module Regfile ( clk, 
+module Regfile ( clk,
 				 rst,
 				 Read_addr_1,
 				 Read_addr_2,
@@ -9,26 +9,48 @@ module Regfile ( clk,
 				 RegWrite,
 				 Write_addr,
 				 Write_data);
-	
+
 	parameter bit_size = 32;
-	
+
 	input  clk, rst;
 	input  [4:0] Read_addr_1;
 	input  [4:0] Read_addr_2;
-	
+
 	output [bit_size-1:0] Read_data_1;
 	output [bit_size-1:0] Read_data_2;
-	
+
 	input  RegWrite;
 	input  [4:0] Write_addr;
 	input  [bit_size-1:0] Write_data;
-	
+
     // write your code in here
-	
+	reg [bit_size-1:0] register [0:31];//32 registers
+
+	assign Read_data_1 = register[Read_addr_1];
+	assign Read_data_2 = register[Read_addr_2];
+
+	integer i;
+	always@(posedge clk, posedge rst)
+	begin
+		if(rst)
+		begin
+			for(i = 0; i < 32; i = i + 1)
+			begin
+				register[i] <= 32'b0;//clear address in every registers
+			end
+		end
+		else
+		begin
+			/****DEBUG****/
+			//for(i = 0; i < 32; i = i + 1)
+			//	$display("%d, %b", i, register[i]);
+			//$display("Write_addr = %b, Write_data = %b\n", Write_addr, Write_data);
+			//$display("$ra = %h\n", register[31]);
+			/****DEBUG****/
+			if(RegWrite == 1 && Write_addr != 0)
+			begin
+				register[Write_addr] <= Write_data;
+			end
+		end
+	end
 endmodule
-
-
-
-
-
-
